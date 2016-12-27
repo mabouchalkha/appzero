9c7e4c51fbcf212505cdcf654ff26a098d282d84
mohamed amine bouchalkha <mabouchalkha@gmail.com>
2016-12-27 11:46:51 -0500

    add api startup

---
+++ api/Controllers/DummyController.cs
@@ -0,0 +1,26 @@
+﻿using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class DummyController : Controller
+    {
+        private CityInfoContext _ctx;
+
+        public DummyController(CityInfoContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        [Route("api/testdatabase")]
+        public IActionResult TestDatabase()
+        {
+            return Ok();
+        }
+    }
+}
